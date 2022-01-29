c718f07bb1b26d7b9e394be9ec8a293753540828
nbuendia <nicole.buendia424@gmail.com>
2022-01-29 18:32:04 -0500

    started footer component - NEEDS WORK

---
+++ client/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//MUI
+import { Box, Typography } from "@mui/material";
+
+function Footer() {
+  return (
+    <Box className='footer'>
+      <Typography>
+        FOOTER!!
+      </Typography>
+    </Box>
+  );
+}
+ export default Footer;

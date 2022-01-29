2e3b1b2793f4524eed0b4f61812ecf97f154213f
nbuendia <nicole.buendia424@gmail.com>
2022-01-29 18:30:31 -0500

    styled card to display proper info, want to add episode but need axios req in home component

--- client/Components/RandomCharacter.js
+++ client/Components/RandomCharacter.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //MUI
-import { Card, CardContent, CardMedia, Container, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 function RandomCharacter(props) {
     const { character } = props;
@@ -10,24 +10,43 @@ function RandomCharacter(props) {
 
     return(
         <>
-            <Container>
-                <Card style={{display: 'flex', background: 'green'}} >
+            <Box className="character-container">
+                <Card className='card' style={{backgroundColor: 'rgb(85, 110, 155)'}}>
                     <CardMedia 
                         component='img'
                         image={character.image}
                         alt={`${character.name} Image`}
-                        style={{width: 250}}
+                        style={{width: 250, height: '100%', borderTopLeftRadius: 10, borderBottomRightRadius: 10, boxShadow: '0 0 10px 5px rgb(50, 65, 95)'}}
                     />
-                    <CardContent>
-                    <Typography>
-                        {character.name}<br/>
-                        {character.gender}<br/>
-                        {character.species}<br/>
-                        {character.status}
-                    </Typography>
+                    <CardContent className="card-content">
+                        <>
+                            <Typography fontWeight={700} fontSize={65} color={'whiteSmoke'} textAlign={'center'}>
+                                <span className="br">
+                                    {character.name}
+                                </span>
+                                <span style={{fontSize: 30}}>
+                                    {character.status}
+                                </span>
+                            </Typography>
+                        </>
+                        <br/>
+                        <Box className="inner-card-content">
+                            <Typography fontWeight={300} fontSize={30} color={'whiteSmoke'} >
+                                <b>GENDER:</b> {character.gender}
+                            </Typography>
+                            <hr className="hr"/>
+                            <Typography fontWeight={300} fontSize={30} color={'whiteSmoke'}>
+                                <b>SPECIES:</b> {character.species}
+                            </Typography>
+                        </Box>
+                        <>
+                            <Typography fontWeight={300} fontSize={30} color={'whiteSmoke'} textAlign={'center'}>
+                                <b>LAST KNOWN LOCATION: </b> {character.location.name}
+                            </Typography>
+                        </>
                     </CardContent>
                 </Card>
-            </Container>
+            </Box>
         </>
     )
 

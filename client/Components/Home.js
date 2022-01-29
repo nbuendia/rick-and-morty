7dcd6b196ea9ea309052e03b1c17795023ddbd56
nbuendia <nicole.buendia424@gmail.com>
2022-01-28 23:52:24 -0500

    got axios information i needed and passed it doen through props to next component, added loading component

--- client/Components/Home.js
+++ client/Components/Home.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+//COMPONENTS
+import Loading from "./Loading";
+import RandomCharacter from "./RandomCharacter";
+
 
 function Home() {
+    const [loading, setLoading] = useState(true);
+    // const [data, getData] = useState(null);
+    const [character, getCharacter] = useState(null);
+    
+    const charactersURL = 'https://rickandmortyapi.com/api/character';
+
+    useEffect(() => {
+        axios.get(charactersURL).then((res) => {
+            // getData(res.data);
+            return res.data;
+        }).then((res) => {
+            axios.get(`${charactersURL}/${Math.floor(Math.random() * res.info.count + 1)}`).then((res) => {
+                getCharacter(res.data)
+                setTimeout(() => {
+                    setLoading(false);
+                }, 3000);
+            })
+        })
+        .catch((error) => {
+            console.error('Oh No? Something Went Wrong!', error)
+        })
+    }, []);
+
+    if (loading) return <Loading />;
+
     return(
-        <h1>HELLO WORLD</h1>
+        <>
+            <RandomCharacter character={character} />
+        </>
     )
 
 }

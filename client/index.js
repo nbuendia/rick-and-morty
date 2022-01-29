dec303bf028557b33be0b14f3a7f860bf285373e
nbuendia <nicole.buendia424@gmail.com>
2022-01-28 23:50:44 -0500

    imported themeProvider and createTheme to change font in mui to imported font

--- client/index.js
+++ client/index.js
@@ -2,11 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
+//MUI
+import { createTheme, ThemeProvider } from "@mui/material";
+
 //COMPONENTS
 import App from './App'
 
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Creepster'
+    }
+});
+
 ReactDOM.render(
     <Router>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </Router>, document.getElementById('root')
 );

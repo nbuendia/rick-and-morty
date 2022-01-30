172b4de9468eeca378e08dfa845cf42645691df7
nbuendia <nicole.buendia424@gmail.com>
2022-01-30 17:41:37 -0500

    clean up

--- client/Components/card/NameAndStatus.js
+++ client/Components/card/NameAndStatus.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //MUI
-import { Typography, Box } from "@mui/material";
+import { Typography } from "@mui/material";
 //MUI ICONS
 import CircleIcon from '@mui/icons-material/Circle';
 
@@ -10,23 +10,23 @@ function NameAndStatus(props) {
 
     return(
         <>
-            <Typography component='div' fontWeight={700} fontSize={65} color={'whiteSmoke'} textAlign={'center'}>
+            <Typography fontWeight={700} fontSize={65} color={'whiteSmoke'} textAlign={'center'}>
                 <span className="block-display line-spacing-5">
                     {character.name}
                 </span>
                 <span className="block-display" style={{fontSize: 30, fontWeight: '300'}}>
                     {character.status === 'Alive' ? 
-                        <Box>
+                        <>
                             <CircleIcon style={{fontSize: '10px', color: 'green'}} /> {character.status}
-                        </Box>
+                        </>
                     : character.status === 'Dead' ? 
-                        <Box>
+                        <>
                             <CircleIcon style={{fontSize: '10px', color: 'red'}} /> {character.status}
-                        </Box>
+                        </>
                     : 
-                        <Box>
+                        <>
                             <CircleIcon style={{fontSize: '10px'}} /> {character.status}
-                        </Box>
+                        </>
                     }
                 </span>
             </Typography>

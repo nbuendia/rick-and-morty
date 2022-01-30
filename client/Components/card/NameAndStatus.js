e68f566bec46d638d2e31e841966dabccca4c7fe
nbuendia <nicole.buendia424@gmail.com>
2022-01-30 17:37:13 -0500

    added icon next to status, green if alive, red if dead, nutural if unknown

--- client/Components/card/NameAndStatus.js
+++ client/Components/card/NameAndStatus.js
@@ -1,19 +1,35 @@
 import React from "react";
 
 //MUI
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+//MUI ICONS
+import CircleIcon from '@mui/icons-material/Circle';
 
 function NameAndStatus(props) {
     const { character } = props;
 
+    console.log(character.status);
+
     return(
         <>
-            <Typography fontWeight={700} fontSize={65} color={'whiteSmoke'} textAlign={'center'}>
+            <Typography component='div' fontWeight={700} fontSize={65} color={'whiteSmoke'} textAlign={'center'}>
                 <span className="block-display line-spacing-5">
                     {character.name}
                 </span>
                 <span className="block-display" style={{fontSize: 30, fontWeight: '300'}}>
-                    {character.status}
+                    {character.status === 'Alive' ? 
+                        <Box>
+                            <CircleIcon style={{fontSize: '10px', color: 'green'}} /> {character.status}
+                        </Box>
+                    : character.status === 'Dead' ? 
+                        <Box>
+                            <CircleIcon style={{fontSize: '10px', color: 'red'}} /> {character.status}
+                        </Box>
+                    : 
+                        <Box>
+                            <CircleIcon style={{fontSize: '10px'}} /> {character.status}
+                        </Box>
+                    }
                 </span>
             </Typography>
         </>

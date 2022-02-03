e3d072ead72de5e907ba88b6bcf1ddeb9e8838b5
nbuendia <nicole.buendia424@gmail.com>
2022-02-02 21:16:33 -0500

    rearranged footer element order to style footer height and have content look better and be responsive to screen size changes

--- client/Components/Footer.js
+++ client/Components/Footer.js
@@ -11,39 +11,41 @@ function Footer() {
   const imgURL = 'https://i.imgur.com/JZXLbjV.png';
 
   return (
-    <Box className='footer'>
-      <Box className="inner-footer-box-1">
-        <Typography fontSize={50} marginRight={'5px'}>
-          Inspired by
-        </Typography>
-
-        <Tooltip title={<Typography fontSize={15}>Rick & Morty API</Typography>} TransitionComponent={Zoom} placement="right">
-          <a href="https://rickandmortyapi.com/" target='_blank' rel="noreferrer noopener">
-            <img src={imgURL} height={45}/>
-          </a>
-        </Tooltip>
-      </Box>
-
+    <Box className='footer' height={{xs: 100, sm: 125, md: 150, lg: 175}}>
       <>
         <a href="https://www.adultswim.com/streams/rick-and-morty" target='_blank' rel="noreferrer noopener">
-          <Typography className="link" fontSize={35} color='black' style={{lineHeight: '1.5'}}>
+          <Typography className="link" fontSize={{xs: 35, sm: 45, md: 55, lg: 65}} color='black' style={{lineHeight: '1'}}>
             Wanna watch some Rick & Morty?
           </Typography>
         </a>
       </>
-
-      <Box className="inner-footer-box-2">
-        <Typography fontSize={30} style={{lineHeight: '1'}}>
-          Find me!
-        </Typography>
-
+      
+      <Box className="inner-footer-box">
         <Box>
-          <a href="https://www.linkedin.com/in/nicole-buendia/" target='_blank' rel="noreferrer noopener">
-            <LinkedInIcon style={{color: '#0e76a8'}} />
-          </a>
-          <a href="https://github.com/nbuendia" target='_blank' rel="noreferrer noopener">
-            <GitHubIcon style={{color: 'black', marginLeft: '10px'}} />
-          </a>
+          <Typography fontSize={{xs: 25, sm: 30, md: 35}} marginRight={'5px'} style={{lineHeight: '1'}}>
+            Inspired by
+          </Typography>
+
+          <Tooltip title={<Typography fontSize={15}>Rick & Morty API</Typography>} TransitionComponent={Zoom} placement="right">
+            <a href="https://rickandmortyapi.com/" target='_blank' rel="noreferrer noopener">
+              <img src={imgURL} height={35}/>
+            </a>
+          </Tooltip>
+        </Box>
+
+        <Box className="icon-box">
+          <Typography fontSize={{xs: 25, sm: 30, md: 35}} style={{lineHeight: '1'}}>
+            Find me!
+          </Typography>
+
+          <Box>
+            <a href="https://www.linkedin.com/in/nicole-buendia/" target='_blank' rel="noreferrer noopener">
+              <LinkedInIcon style={{color: '#0e76a8', fontSize: 35}} />
+            </a>
+            <a href="https://github.com/nbuendia" target='_blank' rel="noreferrer noopener">
+              <GitHubIcon style={{color: 'black', marginLeft: '10px', fontSize: 35}} />
+            </a>
+          </Box>
         </Box>
       </Box>
     </Box>

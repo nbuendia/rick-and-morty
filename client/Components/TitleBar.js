a27718d30c5272748e936a2ab01a37cf66a081db
nbuendia <nicole.buendia424@gmail.com>
2022-02-03 11:02:06 -0500

    testing mobile font size

--- client/Components/TitleBar.js
+++ client/Components/TitleBar.js
@@ -7,7 +7,7 @@ function TitleBar() {
 
   return (
     <Box className="title-bar" height={{xs: 100, sm: 125, md: 150, lg: 175}}>
-      <Typography fontSize={{xs: 40, sm: 55, md: 75, lg: 100}} fontFamily={'creepster'} style={{lineHeight: 1}}>
+      <Typography fontSize={{xs: 35, sm: 50, md: 70, lg: 100}} fontFamily={'creepster'} style={{lineHeight: 1}}>
             RICK & MORTY RAMDOMIZER
         </Typography>
         <Typography fontSize={{xs: 30, sm: 45, md: 60, lg: 80}} fontWeight={700} style={{lineHeight: 1}}>

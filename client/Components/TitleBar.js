012e48203b82b09ec651faa2836f5bca0b1a771f
nbuendia <nicole.buendia424@gmail.com>
2022-02-05 10:01:03 -0500

    removed the -get one r&m character a day line-

--- client/Components/TitleBar.js
+++ client/Components/TitleBar.js
@@ -6,12 +6,9 @@ import { Box, Typography } from "@mui/material";
 function TitleBar() {
 
   return (
-    <Box className="title-bar" height={{xs: 100, sm: 125, md: 150, lg: 175}}>
+    <Box className="title-bar" >
       <Typography fontSize={{xs: 35, sm: 50, md: 70, lg: 100}} fontFamily={'creepster'} style={{lineHeight: 1}}>
             RICK & MORTY RAMDOMIZER
-        </Typography>
-        <Typography fontSize={{xs: 30, sm: 45, md: 60, lg: 80}} fontWeight={700} style={{lineHeight: 1}}>
-            Get one Rick & Morty character a day
         </Typography>
     </Box>
   );

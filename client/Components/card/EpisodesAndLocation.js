3e4d20f3b373ecf0cd582d040102a184c900eb1e
nbuendia <nicole.buendia424@gmail.com>
2022-08-16 11:00:56 -0400

    moved stringConcatination logic into hook, calling hook instead of function

--- client/Components/card/EpisodesAndLocation.js
+++ client/Components/card/EpisodesAndLocation.js
@@ -3,38 +3,12 @@ import React from "react";
 //MUI
 import { Typography } from "@mui/material";
 
+//HOOKS
+import useStringConcatenation from "./hooks/useStringConcatenation";
+
 function EpisodesAndLocation(props) {
     const { character, episodes } = props;
-
-    const episodeNames = episodes.map(episode => episode.name);
-    const seasonEpisodeNum = episodes.map(episode => episode.episode);
-
-    function stringConcatenation (nameArr, detailArr) {
-        if (nameArr.length === 1) return `${nameArr[0]}: ${detailArr[0]}`;
-        else if (nameArr.length === 2) return `${nameArr[0]}: ${detailArr[0]} & ${nameArr[1]}: ${detailArr[1]}`;
-
-        let featuredIn = '';
-        
-        if (nameArr.length > 2) {
-            let lastName = nameArr.pop();
-            let lastDetail = detailArr.pop();
-            let length = nameArr.length > 5 ? 5 : nameArr.length;
-            let last = nameArr.length < 5 ? ` & ${lastName}: ${lastDetail}` : ` ${lastName}: ${lastDetail} & Many More!`;
-
-            //USED 5 TO AVOID A LONG LIST. EX. RICK WOULD BE FEATURED IN EVERY EPISODE
-            for (let i = 0; i < length; i++) {
-                featuredIn += `${nameArr[i]}: ${detailArr[i]}`;
-
-                //IF CHARACTER HAS MORE THAN 5 EPISODES IT ADDS COMMA TILL THE END ELSE B/W ALL WORDS EXCEPT LAST
-                if (nameArr.length > 5) featuredIn += ', ';
-                else if (i < length - 1) featuredIn += ', ';
-            }
-
-            return featuredIn + last;
-        }
-    }
-
-    let featuredInSentence = stringConcatenation(episodeNames, seasonEpisodeNum);
+    const featuredInSentence = useStringConcatenation(episodes);
 
     return(
         <Typography fontWeight={300} fontSize={30} color={'whiteSmoke'} textAlign={'center'} lineHeight={.6} padding={2}>
@@ -52,3 +26,33 @@ function EpisodesAndLocation(props) {
 }
 
 export default EpisodesAndLocation;
+
+// const episodeNames = episodes.map(episode => episode.name);
+// const seasonEpisodeNum = episodes.map(episode => episode.episode);
+
+// function stringConcatenation (nameArr, detailArr) {
+//     if (nameArr.length === 1) return `${nameArr[0]}: ${detailArr[0]}`;
+//     else if (nameArr.length === 2) return `${nameArr[0]}: ${detailArr[0]} & ${nameArr[1]}: ${detailArr[1]}`;
+
+//     let featuredIn = '';
+    
+//     if (nameArr.length > 2) {
+//         let lastName = nameArr.pop();
+//         let lastDetail = detailArr.pop();
+//         let length = nameArr.length > 5 ? 5 : nameArr.length;
+//         let last = nameArr.length < 5 ? ` & ${lastName}: ${lastDetail}` : ` ${lastName}: ${lastDetail} & Many More!`;
+
+//         //USED 5 TO AVOID A LONG LIST. EX. RICK WOULD BE FEATURED IN EVERY EPISODE
+//         for (let i = 0; i < length; i++) {
+//             featuredIn += `${nameArr[i]}: ${detailArr[i]}`;
+
+//             //IF CHARACTER HAS MORE THAN 5 EPISODES IT ADDS COMMA TILL THE END ELSE B/W ALL WORDS EXCEPT LAST
+//             if (nameArr.length > 5) featuredIn += ', ';
+//             else if (i < length - 1) featuredIn += ', ';
+//         }
+
+//         return featuredIn + last;
+//     }
+// }
+
+// let featuredInSentence = stringConcatenation(episodeNames, seasonEpisodeNum);
